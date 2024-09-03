cbc5fd877a995d306498ed931f98d2c5ebfaa8c7
Fei.Li <leef863@163.com>
2024-09-03 10:56:42 +0800

    Initial commit

---
+++ STM32Cube_FW_F1_V1.0.0RC1/CMSIS/Device/ST/STM32L4xx/Include/stm32l4xxxxx-generate-replace.bat
@@ -0,0 +1,20 @@
+@color 0B
+@echo off
+
+REM Generate all CMSIS files 
+REM Active perl must be installed
+
+set scriptPATH=%CD%\..\..\..\..\..\..\__INTERNAL__Validation_Integration\Integration_Tool\_CmsisDeviceGenerator\
+
+if not exist "%scriptPATH%"  (
+	echo Input directory does not exist!
+	pause
+	exit
+)
+
+cd %scriptPATH%
+
+DeviceGeneration.pl  --target STM32L4xx -replace
+
+pause
+:EOF
